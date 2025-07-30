cbd3c6fae337f1ba9d35984d6fee656e1c153193
IsaacTatt <icehatgamers@gmail.com>
2025-07-30 19:39:49 +1000

    UI label setup

--- lunchrush/scripts/ui_manager.gd
+++ lunchrush/scripts/ui_manager.gd
@@ -1,1 +1,17 @@
 extends Node
+
+@onready var MoneyLabel: Label = $HBoxContainer/TextureRect/HBoxContainer/MarginContainer/MoneyLabel
+@onready var HealthLabel: Label = $HBoxContainer/TextureRect2/HBoxContainer/MarginContainer/HealthLabel
+
+func _ready():
+	updateLabels()
+
+func updateLabels():
+	updateLabelMoney()
+	updateLabelHealth()
+
+func updateLabelMoney():
+	MoneyLabel.text = "$" + str(GameStateManager.current_money)
+
+func updateLabelHealth():
+	HealthLabel.text = str(GameStateManager.current_hp) + "/" + str(GameStateManager.max_hp)
