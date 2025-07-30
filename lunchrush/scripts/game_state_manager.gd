0375497fae3fa238a87ae81cdd95472182f352f9
Home\jacin_tatt <jacin@piculia.net>
2025-07-30 19:43:49 +1000

    added signals

--- lunchrush/scripts/game_state_manager.gd
+++ lunchrush/scripts/game_state_manager.gd
@@ -1,6 +1,8 @@
 extends Node
 
 signal hp_changed(hp)
+signal money_changed(money)
+signal wave_changed(wave)
 
 var max_hp: int = 100
 var current_hp: int:
@@ -8,7 +10,13 @@ var current_hp: int:
 		current_hp = clamp(value, 0, max_hp)
 		hp_changed.emit(current_hp)
 
-var current_money: int = 100
+var current_money: int = 100:
+	set(value):
+		money_changed.emit(current_money)
+		
+var current_wave: int = 0:
+	set(value):
+		wave_changed.emit(current_wave)
 
 func _ready():
 	current_hp = max_hp
